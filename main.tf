bb83eed037c1e3b1b114fdfdab8929656f819106
Mohamed Kharraz <mohamedkharraz@yahoo.com>
2026-05-26 18:40:37 +0200

    ArgoCD module init

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "kubernetes" {
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", var.kubernetes_cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    # Added '=' here because it is inside an object map
+    exec = {
+      api_version = "client.authentication.k8s.io/v1"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", var.kubernetes_cluster_name]
+    }
+  }
+}
+
+resource "kubernetes_namespace" "argo-ns" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "msur"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = kubernetes_namespace.argo-ns.metadata[0].name
+}
